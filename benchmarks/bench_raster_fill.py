693f8986bdd5da3c9237d59d4c4499a622895d19
mcflugen <mcflugen@gmail.com>
2024-09-03 12:41:37 -0600

    benchmark calc_area_of_patch, calc_centroid_of_patch

--- benchmarks/bench_raster_fill.py
+++ benchmarks/bench_raster_fill.py
@@ -1,5 +1,7 @@
 import numpy as np
 from landlab.graph.quantity.ext.of_link import calc_midpoint_of_link
+from landlab.graph.quantity.ext.of_patch import calc_area_at_patch
+from landlab.graph.quantity.ext.of_patch import calc_centroid_at_patch
 from landlab.graph.structured_quad.ext.at_cell import fill_node_at_cell
 from landlab.graph.structured_quad.ext.at_face import fill_nodes_at_face
 from landlab.graph.structured_quad.ext.at_link import fill_nodes_at_link
@@ -33,14 +35,19 @@ def number_of_cells(shape):
 
 class TimeGraphQuantity:
     params = [16, 32, 64, 128, 256, 512, 1024, 2048]
+    param_names = ["n"]
 
     def setup(self, n):
         graph = UniformRectilinearGraph((n, n))
 
         self._nodes_at_link = np.empty((number_of_links((n, n)), 2), dtype=int)
         fill_nodes_at_link((n, n), self._nodes_at_link)
-        self._x_of_node = graph.x_of_node
-        self._y_of_node = graph.y_of_node
+        self._x_of_node = graph.x_of_node.copy()
+        self._y_of_node = graph.y_of_node.copy()
+        self._nodes_at_patch = graph.nodes_at_patch
+        self._links_at_patch = graph.links_at_patch
+        self._x_of_link = graph.xy_of_link[:, 0].copy()
+        self._y_of_link = graph.xy_of_link[:, 1].copy()
 
     def time_xy_of_link(self, n):
         out = np.empty((number_of_links((n, n)), 2), dtype=float)
@@ -51,9 +58,27 @@ class TimeGraphQuantity:
             out,
         )
 
+    def time_area_of_patch(self, n):
+        out = np.empty(number_of_patches((n, n)), dtype=float)
+        calc_area_at_patch(
+            self._nodes_at_patch,
+            self._x_of_node,
+            self._y_of_node,
+            out,
+        )
+
+    def time_centroid_of_patch(self, n):
+        out = np.empty((number_of_patches((n, n)), 2), dtype=float)
+        calc_centroid_at_patch(
+            self._links_at_patch,
+            self._x_of_link,
+            self._y_of_link,
+            out,
+        )
 
 class TimeRasterFill:
     params = [16, 32, 64, 128, 256, 512, 1024, 2048]
+    param_names = ["n"]
 
     def time_node_at_cell(self, n):
         out = np.empty(number_of_cells((n, n)), dtype=int)
